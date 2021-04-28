25efbf2ccc0b3729c1c9650870dc775a4d2a554d
RicardoFagundes123 <rlf4@cin.ufpe.br>
2021-04-28 16:37:54 -0300

    Remove comments App

--- App.tsx
+++ App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-// import {
-//   useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold,
-// } from '@expo-google-fonts/roboto';
-// import { Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import {
+  useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Nunito_400Regular, Nunito_600SemiBold, Nunito_700Bold } from '@expo-google-fonts/nunito';
 
 
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "react-native";
 
-import { Caregiver,Home,Login } from "./src/pages";
+import { Caregiver, Login} from "./src/pages";
 
 const App: React.FC = () => {
-  // const [fontsLoaded] = useFonts({
-  //   Roboto_400Regular,
-  //   Roboto_500Medium,
-  //   Roboto_700Bold,
-  //   Nunito_400Regular,
-  //   Nunito_600SemiBold,
-  //   Nunito_700Bold,
-  // });
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Nunito_400Regular,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+  });
 
-  // if (!fontsLoaded) {
-  //   return <AppLoading />;
-  // }
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return (
     <>
