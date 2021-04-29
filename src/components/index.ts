6d2534aac4f73b46855dec52e264fd5034fa2544
tapachec0 <tmrp@cin.ufpe.br>
2021-04-28 23:23:52 -0300

    feat(caregiver-profile): adding media section and creating and adding AddMediaComponent

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,6 +13,7 @@ import PresentCardComponent from './PresentCardComponent'
 import InsertImageComponent from './InsertImageComponent'
 import DescriptionEditableComponent from './CaregiverEditableDescriptionComponent'
 import EditButtonComponent from './EditButtonComponent'
+import AddMediaComponent from './AddMediaComponent'
 
 
-export { ButtonComponent, EditButtonComponent, EvaluationComponent, DescriptionEditableComponent, ConfirmationComponent, ComplimentsComponent, EvaluationButtonComponent, Screen, CaregiverDescriptionComponent, Divider, PlantComponent, AddPlantComponent, AddPlantButtonComponent, PresentCardComponent, InsertImageComponent };
+export { ButtonComponent, AddMediaComponent, EditButtonComponent, EvaluationComponent, DescriptionEditableComponent, ConfirmationComponent, ComplimentsComponent, EvaluationButtonComponent, Screen, CaregiverDescriptionComponent, Divider, PlantComponent, AddPlantComponent, AddPlantButtonComponent, PresentCardComponent, InsertImageComponent };
