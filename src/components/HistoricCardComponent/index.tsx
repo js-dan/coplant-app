f35ec7c42c2e639aa1b4900c95c5839bb66953fe
GabrielCavalcanti13 <gcm2@cin.ufpe.br>
2021-04-29 23:08:07 -0300

    feat(historic-page): Adjusting page to change related to the props

--- src/components/HistoricCardComponent/index.tsx
+++ src/components/HistoricCardComponent/index.tsx
@@ -7,9 +7,14 @@ import {
   HistoricCardStar,
   HistoricCardTitle,
   HistoricCardEndDate,
+  HistoricCardInfoContainer,
   HistoricCardTitleContainer,
   UserImage,
-  HistoricCardFinishedText
+  DaysRemainingText,
+  DaysRemainingContainer,
+  DaysRemainingNumber,
+  HistoricCardStatus,
+  HistoricCardMessage
 } from "./style";
 
 import theme from "../../assets/theme";
@@ -19,34 +24,69 @@ import { Divider } from "../DividerComponent/style";
 export interface HistoricCardComponentProps {
   userName?: string;
   endDate?:Date;
-  finished?: boolean
-  rated?: boolean
+  finished?: boolean;
+  rated?: boolean;
+  remainingDays?: number;
 }
 
 const HistoricCardComponent: React.ElementType<HistoricCardComponentProps> = ({
   userName,
   endDate,
   finished,
-  rated
+  rated,
+  remainingDays
 }: HistoricCardComponentProps) => {
-  return (
-    <HistoricCardContainer>
-      <UserImage source={require('../../assets/img/users/'+userName+'.png')}/>
-      <HistoricCardTitleContainer>
-        <HistoricCardTitle>{userName}</HistoricCardTitle>
-        <HistoricCardRating>
-          <HistoricCardText>Sua Nota: </HistoricCardText>
-          <HistoricCardStar source={icons.socialMedia.star} />
-          <HistoricCardStar source={icons.socialMedia.star} />
-          <HistoricCardStar source={icons.socialMedia.star} />
-          <HistoricCardStar source={icons.socialMedia.star} />
-          <HistoricCardStar source={icons.socialMedia.star} />
-        </HistoricCardRating>
-        <HistoricCardEndDate>{" Finalizado em: "+endDate.toLocaleDateString()}</HistoricCardEndDate>
-      </HistoricCardTitleContainer>
-      
-    </HistoricCardContainer>
-  );
+  if (finished && rated) {
+    return (
+      <HistoricCardContainer>
+        <UserImage source={require('../../assets/img/users/'+userName+'.png')}/>
+        <HistoricCardInfoContainer>
+          <HistoricCardTitle>{userName}</HistoricCardTitle>
+          <HistoricCardRating>
+            <HistoricCardText>Sua Nota: </HistoricCardText>
+            <HistoricCardStar source={icons.socialMedia.star} />
+            <HistoricCardStar source={icons.socialMedia.star} />
+            <HistoricCardStar source={icons.socialMedia.star} />
+            <HistoricCardStar source={icons.socialMedia.star} />
+            <HistoricCardStar source={icons.socialMedia.star} />
+          </HistoricCardRating>
+          <HistoricCardEndDate>{"Finalizado em: "+endDate.toLocaleDateString()}</HistoricCardEndDate>
+        </HistoricCardInfoContainer>
+      </HistoricCardContainer>
+    );
+  };
+  if (finished && !rated) {
+    return (
+      <HistoricCardContainer>
+        <UserImage source={require('../../assets/img/users/'+userName+'.png')}/>
+        <HistoricCardInfoContainer>
+          <HistoricCardTitle>{userName}</HistoricCardTitle>
+          <HistoricCardRating>
+            <HistoricCardText>Sua Nota: </HistoricCardText>
+            <HistoricCardMessage>Avalie agora</HistoricCardMessage>
+          </HistoricCardRating>
+          <HistoricCardEndDate>{"Finalizado em: "+endDate.toLocaleDateString()}</HistoricCardEndDate>
+        </HistoricCardInfoContainer>
+      </HistoricCardContainer>
+    );
+  };
+  if (!finished) {
+    return (
+      <HistoricCardContainer>
+        <UserImage source={require('../../assets/img/users/'+userName+'.png')}/>
+        <HistoricCardInfoContainer>
+          <HistoricCardTitleContainer>
+            <HistoricCardTitle>{userName}</HistoricCardTitle>
+            <HistoricCardStatus></HistoricCardStatus>
+          </HistoricCardTitleContainer>
+          <DaysRemainingContainer>
+            <DaysRemainingNumber>{remainingDays.toString()}</DaysRemainingNumber>
+            <DaysRemainingText>{" dias restantes"}</DaysRemainingText>
+          </DaysRemainingContainer>
+        </HistoricCardInfoContainer>
+      </HistoricCardContainer>
+    );
+  };
 };
 
 export default HistoricCardComponent;
